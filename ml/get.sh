7fa95f400b099c8ebecc14278f13fadc3a0f318c
makevoid <makevoid@gmail.com>
2019-05-10 06:01:26 +0100

    use bitstamp btc usd price in the example

--- ml/get.sh
+++ ml/get.sh
@@ -1,3 +1,6 @@
-wget https://api.bitcoincharts.com/v1/csv/bitfinexUSD.csv.gz
-gunzip bitfinexUSD.csv.gz
-mv bitfinexUSD.csv data.csv
+wget https://api.bitcoincharts.com/v1/csv/bitstampUSD.csv.gz
+gunzip bitstampUSD.csv.gz
+
+# Cut only latest period
+# tail -20900000 bitstampUSD.csv > data.csv
+tail -100000 bitstampUSD.csv > data.csv
