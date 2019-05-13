cba32b92ca8d64bf063c38e9bac258e93a52b23e
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2019-05-13 11:42:49 +0100

    force overwrite

--- ml/get.sh
+++ ml/get.sh
@@ -1,5 +1,5 @@
-wget https://api.bitcoincharts.com/v1/csv/bitstampUSD.csv.gz
-gunzip bitstampUSD.csv.gz
+curl https://api.bitcoincharts.com/v1/csv/bitstampUSD.csv.gz > bitstampUSD.csv.gz
+gunzip -f bitstampUSD.csv.gz
 
 # Cut only latest period (from previous cycle high)
 tail -20900000 bitstampUSD.csv > data.csv
