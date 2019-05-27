ac9c2069022e3f06157840f6d939539dc3a8490d
makevoid <makevoid@gmail.com>
2019-05-27 02:44:53 +0100

    modify price range timeline

--- ml/get.sh
+++ ml/get.sh
@@ -4,7 +4,8 @@ curl https://api.bitcoincharts.com/v1/csv/bitstampUSD.csv.gz > bitstampUSD.csv.g
 gunzip -f bitstampUSD.csv.gz
 
 # Cut only latest period (from previous cycle high)
-tail -20900000 bitstampUSD.csv > data.csv
+# tail -20900000 bitstampUSD.csv > data.csv
+tail -17000000 bitstampUSD.csv > data.csv
 
 # Cut only latest period (from this year)
 # tail -12000000 bitstampUSD.csv > data.csv
